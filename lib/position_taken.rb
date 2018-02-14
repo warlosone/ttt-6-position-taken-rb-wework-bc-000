677170de93000a64674ba916fbb3317bd2f03849
Mars Santos <mars@meetup.com>
2018-02-14 01:09:09 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+ if board[index] == "X" || board[index] == "O" 
+   true
+ else 
+   false
+ end
+end
+
+ #NOT FAIL METHOD
+ #if board[index] == " " || board[index] =="" || board[index]== nil
+   # false
+  #else
+  #  true
+  #end
